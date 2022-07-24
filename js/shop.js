6751e0634fd4b0b34dbaf98d66037aec0570e70f
NoeliaAlvarez <noelia.ag1990@gmail.com>
2022-07-24 16:12:21 +0200

    Sprint 3 Nivell 1 Exercici 2 Buidar carret

--- js/shop.js
+++ js/shop.js
@@ -75,12 +75,11 @@ var total = 0;
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     let found = false;
-    
+
     for (let i = 0; i < products.length && found === false; i++) {
         if (i === id - 1) {
             cartList.push(products[i]);
             found = true;
-            console.log(cartList);
         }
     }
     /*cartList.push(products[id -1]);
@@ -90,7 +89,7 @@ function buy(id) {
 
 // Exercise 2
 function cleanCart() {
-
+    cartList = [];
 }
 
 // Exercise 3
